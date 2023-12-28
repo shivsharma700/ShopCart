68f599dc5000ca93bb781d26e4f48c5321389e16
shiv <mansotra.shiv@gmail.com>
2023-12-28 16:23:58 +0530

    edited footer import in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 
 import Header from './components/Header/Header'
-import Footer from './components/footer/Footer'
+import Footer from './components/Footer/Footer'
 import MainRoutes from './routes/MainRoutes'
 import UserContext from './context/UserContext'
 import CartContext from "./context/CartContext";
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 function Footer() {
     return (
-        <footer 
+        <Footer 
             // style={{marginTop: '10rem'}} 
             className="footer py-3 bg-body-tertiary"
         >
             <div className="container d-flex justify-content-center" >
                 <span className="text-body-secondary">ShopCart</span>
             </div>
-        </footer>
+        </Footer>
     )
 }
 
